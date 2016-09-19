e89f5f50c5d256753fc044694ac4d7002049eaa6
Jan Ghods <ghods.jan@gmail.com>
2016-09-19 20:56:50 +0200

    ToDooo Start

---
+++ ToDooo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDooo
+//
+//  Created by Jan Ghods on 19.09.16.
+//  Copyright © 2016 Jan Ghods. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
